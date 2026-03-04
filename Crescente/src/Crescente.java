4c4d3c17b9c193c3a401c17f5bbbbab4b2b101d3
DanLus1 <samu.nico837@gmail.com>
2026-03-03 23:00:04 -0300

    Update Crescente.java

--- Crescente/src/Crescente.java
+++ Crescente/src/Crescente.java
@@ -1,3 +1,7 @@
+// Problema: Crescente ou Decrescente
+// Leia uma quantidade indeterminada de duplas de valores inteiros x e y. Para cada dupla, o programa deve imprimir uma mensagem indicando se esses valores foram digitados em ordem Crescente ou Decrescente. Caso os 
+// valores sejam iguais, o programa deve encerrar a leitura de dados e ser finalizado.
+
 import java.util.Locale;
 import java.util.Scanner;
 
